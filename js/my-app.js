06dbbc20aaf6f3732e4733ef2fcff69847f4de9b
José Yvimas <jyvimas@gmail.com>
2018-01-30 00:48:55 -0400

    Laboratorio F7

---
+++ js/my-app.js
@@ -0,0 +1,17 @@
+
+// Inicializamos la aplicación
+var myApp = new Framework7({});
+
+// Bibloteca manejadora del DOM
+var $$ = Dom7;
+
+// Añadimos una vista
+var mainView = myApp.addView('.view-main', {
+    dynamicNavbar: true
+});
+
+
+myApp.onPageInit('',function(page){
+});
+
+
